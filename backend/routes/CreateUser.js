71df50dd420209c7ab93dd5df66f41d79fe4adbf
parulsurana <parulsurana28@gmail.com>
2023-03-30 18:23:49 +0530

    Done with the authentication and authorization.

--- backend/routes/CreateUser.js
+++ backend/routes/CreateUser.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const User = require("../models/User.js");
 const { body, validationResult } = require("express-validator");
 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtSecret =
+  "Thisisthemernstackproject@havingauthenticationandauthorization";
+
 router.post(
   "/createuser",
   [
@@ -17,10 +22,14 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
+    const jwt = require("jsonwebtoken");
+    const salt = await bcrypt.genSalt(10);
+    let secPassword = await bcrypt.hash(req.body.password, salt);
+
     try {
       await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password: secPassword,
         email: req.body.email,
         location: req.body.location,
       }).then(res.json({ success: true }));
@@ -52,12 +61,26 @@ router.post(
           .json({ errors: "Try login through right credentials." });
       }
 
-      if (req.body.password !== userData.password) {
+      const pwdCompare = await bcrypt.compare(
+        req.body.password,
+        userData.password
+      );
+
+      if (!pwdCompare) {
         return res.status(400).json({
           errors: "Try login through right credentials. Password error",
         });
       }
-      return res.json({ success: true });
+
+      const data = {
+        user: {
+          id: userData.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, jwtSecret);
+
+      return res.json({ success: true, authToken: authToken });
     } catch (error) {
       console.log(error);
       res.json({ success: false });
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -27,6 +27,8 @@ export default function Login() {
 
     if (!json.success) alert("Enter Valid Credentials");
     if (json.success) {
+      localStorage.setItem("authToken", json.authToken);
+      console.log(localStorage.getItem("authToken"));
       navigate("/");
     }
   };
