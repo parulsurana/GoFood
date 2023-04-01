03ee618bd54603d87d50827066516146586bec93
parulsurana <parulsurana28@gmail.com>
2023-04-01 15:38:42 +0530

    Added Login and Logout functionality.

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,12 @@ export default function Card(props) {
     <div>
       {" "}
       <div className="card mt-3 Card">
-        <img src={props.imgSrc} className="card-img-top" alt="..." />
+        <img
+          src={props.imgSrc}
+          className="card-img-top"
+          alt="..."
+          style={{ height: "120px", objectFit: "fill" }}
+        />
         <div className="card-body">
           <h5 className="card-title">{props.foodName}</h5>
           <div className="container w-100">
